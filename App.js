ec698107f68d7dc6f33c6d9af87728d65913e977
akhzar nazir <akhzarn@yahoo.com>
2019-01-08 23:32:04 +0500

    New Commit

--- App.js
+++ App.js
@@ -7,131 +7,73 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Alert, Button,
+import {Platform, StyleSheet, Text, View, Alert,AsyncStorage, Button,
         } from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
 
-  // Read from the given Link
-  // https://aboutreact.com/react-native-application-life-cycle-of/
-
   constructor() {
-
-    // Construction will run at first
-
     super();
+
     this.state={
-        // bookArray:['hasnain','hasan','ABC'],
+        // bookArray:[56,78,90,98,100],
         // newVar:'0'
       }
   }
 
   componentDidMount() {
 
-    // After Constructor ComponentDidmountwillRun
-
+  AsyncStorage.getItem("login").then((value) => {
+  if (value == null) {
+    AsyncStorage.setItem('login', 'abc');
+  }else{
   }
+}
+).done();
 
-  componentWillMount() {
+AsyncStorage.getItem("password").then((value) => {
+if (value == null) {
+  AsyncStorage.setItem('password', '123');
+}else{
+}
 
-  // Note: Before running ComponentWillMount render() function will be called
-
+}
+).done();
+    // this.yasirFunction();
   }
-
-    componentWillReceiveProps(nextProp) {
-      // Alert.alert('componentWillReceiveProps');
-      console.log('componentWillReceiveProps called.', nextProp);
-    }
-
-    // Called 1
-    shouldComponentUpdate(nextProp, nextState) {
-      // Alert.alert('shouldComponentUpdate');
-      console.log('shouldComponentUpdate called.');
-      return true;
-    }
-
-    // Called 2
-    componentWillUpdate(nextProp, nextState) {
-      // Alert.alert('componentWillUpdate');
-      console.log('componentWillUpdate called.');
-    }
-
-    componentDidUpdate(prevProp, prevState) {
-      // Alert.alert('componentDidUpdate');
-      console.log('componentDidUpdate called.');
-    }
-
-    componentWillUnmount() {
-      Alert.alert('componentWillUnmount');
-      console.log('componentWillUnmount called.');
-    }
-
-    componentDidCatch(error, info) {
-      Alert.alert('componentDidCatch');
-      console.log('componentDidCatch called.');
-    }
 
   render() {
     return (
-
-  <View style={styles.container}>
-
-  <View style={styles.viewStyleOne}>
-    <Text style={styles.textStyle}> ABC </Text>
-  </View>
-
-  <View style={styles.viewStyleTwo}>
-    <Text style={styles.textStyle}> XYZ </Text>
-  </View>
-
-  </View>
-
+      <View style={styles.container}>
+        <Text style={styles.welcome}> Test AsyncStorage </Text>
+      </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
-
-  container:{
-    backgroundColor:'#4286f4',
-    flex:1,
-    flexDirection:'row',
-    },
-
-  textStyle:{
-    textAlign:'center'
-  },
-
-  viewStyleOne: {
-    width:40,
-    height:200,
+  container: {
+    flex: 1,
     justifyContent: 'center',
-    alignItems:'center',
-    backgroundColor:'red',
-    flex:500,
-    // flexGrow: 1,
-    // alignSelf: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-
-  viewStyleTwo: {
-    width:40,
-    height:200,
-    justifyContent: 'center',
-    alignItems:'center',
-    backgroundColor:'#b642f4',
-    flex:500,
-    // flexGrow: 1,
-    // alignSelf: 'flex-start',
+  textStyleNP:{
+  color:'white',
+  fontSize:25,
+  backgroundColor:'black',
+  // fontWeight:'bold',
+},
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
-  viewStyleThree: {
-    width:40,
-    height:40,
-    justifyContent: 'center',
-    alignItems:'center',
-    backgroundColor:'green',
-    flex:1,
-    // flexGrow: 1,
-    // alignSelf: 'flex-start',
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
